ba8ae699bc9efd560f216d6f111379e4d9ac3faa
sumit-singhania <sumit.singhania@sourcefuse.com>
2022-07-15 17:53:51 +0530

    added enumms

---
+++ src/app/model/user-role-enums.ts
@@ -0,0 +1,6 @@
+export enum UserRoleEnums {
+    subscriber = 'SUSCRIBER',
+    admin = 'ADMIN',
+    superAdmin = 'SUPERADMIN'
+
+}
--- src/app/services/user-list.service.ts
+++ src/app/services/user-list.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { UserList } from '../model/user-list';
+import { UserRoleEnums } from '../model/user-role-enums';
 
 @Injectable({
   providedIn: 'root'
@@ -19,7 +20,7 @@ export class UserListService {
       LastName: 'Singhania', 
       email: 'sumitsinghania68@gmail.com', 
       phoneNumber: 7508799790, 
-      role: 'user', 
+      role: UserRoleEnums.subscriber, 
       address: 'na', 
       createdOn: '20/02/2020', 
       modifiedOn: '20/02/2020',
@@ -32,7 +33,7 @@ export class UserListService {
       LastName: 'Singhania', 
       email: 'sumitsinghania68@gmail.com', 
       phoneNumber: 7508799790, 
-      role: 'user', 
+      role: UserRoleEnums.admin, 
       address: 'na', 
       createdOn: '20/02/2020', 
       modifiedOn: '20/02/2020',
